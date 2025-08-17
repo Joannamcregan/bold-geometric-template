fd54cea0cb597ebc6bd782ed4a93927fd72f2aa3
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-08-17 13:05:52 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,21 @@
-import u from '/umbrella.esm.js';
-const selections = u('.selection');
-const seeMores = u('.see-more');
-const seeLesses = u('.see-less');
-const body = u('body');
-const footer = u('footer');
-selections.on('click', (e)=>{
-    const selected = u(e.target);
-    selections.removeClass('selected-glasses');
-    setTimeout((e)=>{
-        selected.addClass('selected-glasses');
-        body.removeClass('light dark sepia');
-        footer.removeClass('light dark sepia');
-        body.addClass(selected.data('mode'));
-        footer.addClass(selected.data('mode'));
-    }, 100)
-})
-seeMores.on('click', (e)=>{
-    u(e.target).parent('.description-text').find('.expanded-description').removeClass('hidden');
-    u(e.target).parent('.description-text').find('.see-less').removeClass('hidden');
-    u(e.target).addClass('hidden');
-});
-seeLesses.on('click', (e)=>{
-    u(e.target).parent('.description-text').find('.expanded-description').addClass('hidden');
-    u(e.target).parent('.description-text').find('.see-more').removeClass('hidden');
-    u(e.target).addClass('hidden');
-});
+window.onload = scrollMarquee();
+
+function scrollMarquee() {
+    const marquee = document.getElementById('marquee');
+    const clone = marquee.innerHTML;
+    const firstElement = marquee.children[0];
+    marquee.insertAdjacentHTML('beforeend', clone);
+    marquee.insertAdjacentHTML('beforeend', clone);
+    marquee.insertAdjacentHTML('beforeend', clone);
+    marquee.insertAdjacentHTML('beforeend', clone);
+    marquee.insertAdjacentHTML('beforeend', clone);
+    marquee.insertAdjacentHTML('beforeend', clone);
+    let i = 0;
+    setInterval(function () {
+        firstElement.style.marginLeft = `-${i}px`;
+        if (i > firstElement.clientWidth) {
+            i = 0;
+        }
+        i = i + 0.2;
+    }, 0);
+}
